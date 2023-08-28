e975881446cc44ad5dcfb083e04a78adec3d26ff
koredebanfe <oluwakoredebanfe@gmail.com>
2023-08-27 17:38:13 -0700

    feat:adding responsiveness to the page

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const mapList = document.querySelector(".map--list");
 const postcode = document.querySelector(".postcode");
 const postcodeList = document.querySelector(".postcode--list");
 
+const navOpen = document.querySelector(".nav--open");
+const navContent = document.querySelector(".nav--content");
+
 postcode.addEventListener("click", ()=>
 postcodeList.classList.toggle("show--list"))
 
 map.addEventListener("click", ()=>
 mapList.classList.toggle("show--list"))
+
+
+navOpen.addEventListener("click", ()=>
+navContent.classList.toggle("show--list"))
